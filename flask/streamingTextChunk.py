e0a0ac71f9b9326037bf8857b3d4b3dd92c70ec6
Your Name <you@example.com>
2024-06-06 12:20:02 +0900

    update multilang

--- flask/streamingTextChunk.py
+++ flask/streamingTextChunk.py
@@ -2,10 +2,12 @@ from flask import Flask, request, Response, jsonify
 from flask_cors import CORS
 from pydub import AudioSegment
 import io
+import os
 # import langid
 from openai import OpenAI
 from chunkingText import TextCutter
 from rag_class import RagInterface
+import deepl
 
 AudioSegment.converter = "D:/ffmpeg-7.0.1-full_build/bin/ffmpeg.exe"
 
@@ -15,16 +17,31 @@ CORS(app)
 # OpenAI Settings
 api_key = ""
 client = OpenAI(api_key=api_key)
-cutter = TextCutter(min_chars=15)
+cutter = TextCutter(min_chars=11)
 rag_processor = RagInterface('demo.docx', 'index0', './index')
 
-def generate_text_stream(query):
-    prompt, title = rag_processor.process_input(query)
+auth_key = ""
+translator = deepl.Translator(auth_key)
+
+
+templates = {
+    'ZH': '请扮演一个智能助手，基于背景，简练且严谨地回答问题。',
+    'JA': '背景に基づいて簡潔かつ正確に質問に答えるインテリジェントアシスタントを演じてください。',
+    'EN': 'Please act as an intelligent assistant and answer questions concisely and accurately based on the background.'
+}
+
+folder_path = "../extracted_images"
+# global images_list
+images_list = os.listdir(folder_path)
+
+def generate_text_stream(prompt, language):
+    
+    template = templates.get(language, 'Please act as an intelligent assistant and answer questions concisely and accurately based on the background.')
     
     for chunk in client.chat.completions.create(
         model="gpt-4o",
         messages=[
-            {'role': 'system', 'content': '请扮演一个智能助手，佑妮，基于背景，简练且严谨地回答问题。'},
+            {'role': 'system', 'content': template},
             {'role': 'user', 'content': prompt},
         ],
         stream=True
@@ -64,16 +81,29 @@ def process_audio():
     result = transcribe(client, request)
     transcription = result.text
     print("Transcript:", transcription)
-
+    
+    # 如果DeepL速度慢，可以考虑换ASR模型直接翻译
+    language = request.form.get('language')
+    main_lang = "ZH"
+    if language == main_lang:
+        translation_result = translator.translate_text(transcription, target_lang=main_lang)
+        print("Translated Transcript:", translation_result.text)
+        transcription = translation_result.text
+    
+    # DeepL也能做语言类型识别
     # lang, _ = langid.classify(transcription)
     # print("Language:", lang)
 
+    prompt, title = rag_processor.process_input(transcription, language)
+    
+    if title not in images_list:
+        title = 'N'
+    
     def response_stream():
         # yield f"Detected Language: {lang}\n\n"
-        
+        yield f"{title} |"
         # yield from cutter.cut_text(generate_text_stream(transcription))
-        
-        for chunk in cutter.cut_text(generate_text_stream(transcription)):
+        for chunk in cutter.cut_text(generate_text_stream(prompt, language)):
             print(f"Chunk sent: {chunk}")
             yield chunk
 
